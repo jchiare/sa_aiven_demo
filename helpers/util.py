0cf4271a12d9c1bfeeeeff2d9880d32b96fbe9ff
Jay Chiarella <jay.gch93@gmail.com>
2021-01-30 19:34:10 +0100

    [main] add comments
    on branch [main]

--- helpers/util.py
+++ helpers/util.py
@@ -1,11 +1,20 @@
+# Utility functions 
+
 from datetime import datetime
 from socket import gethostname
 from typing import Text
 
 
 def get_UTC_time() -> datetime:
+    """
+    Returns current time in UTC 
+    """
     return datetime.now().isoformat()
 
 
 def get_host_name() -> Text:
+    """
+    Returns the host name of the machine calling this function 
+    - compatible with Linux, Mac, and Windows
+    """
     return gethostname()
